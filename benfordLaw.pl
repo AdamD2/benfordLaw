c8fc18d1acb06774b0735914b2cb4833d8bd47fb
AdamD2 <adam_douglas2@hotmail.com>
2017-06-05 19:33:12 +1000

    Fixed perl version so that it performs the basic functionality.

--- benfordLaw.pl
+++ benfordLaw.pl
@@ -9,20 +9,24 @@ main();
 
 sub main {
     my %num_count;
+
+    # Initialize the hash to zero
     for (my $i = 1; $i <= 9; $i++) {
         $num_count{$i} = 0;
     }
 
+    # Load in each number
     foreach $num (<>) {
         $indexes++;
 
-        if ($num =~ /[1-9]/) {
+        if ($num =~ /^[1-9]/) {
             $num =~ s/^([1-9]).*$/$1/;
-            print $num;
+            $num =~ s/\s*$//;
             $num_count{$num} = $num_count{$num} + 1;
         }
     }
 
+    # Print distributions for each number
     for (my $i = 1; $i <= 9; $i++) {
         $distribution = ($num_count{$i}/$indexes)*100;
         print "Distribution for $i is $distribution\n";
